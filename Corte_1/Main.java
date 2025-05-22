e71acdcad4559f711c8ca8e85e9025f4df77cd7f
NicolasR-UD <nicojkl10@gmail.com>
2025-05-22 12:36:42 -0500

    Add files via upload

---
+++ Corte_1/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        Operations resultado = new Operations();
+        resultado.sumar (3,2);
+        System.out.println("la suma es igual a:"+resultado.r);
+
+        
+        resultado.restar (5,2);
+        System.out.println("la suma es igual a:"+resultado.r);
+
+        
+        resultado.multiplicar (3,2);
+        System.out.println("la suma es igual a:"+resultado.r);
+
+        
+        resultado.potencia (3,2);
+        System.out.println("la suma es igual a:"+resultado.r);
+
+        
+    }
+    
+}
